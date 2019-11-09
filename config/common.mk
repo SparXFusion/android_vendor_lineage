29c6c7b7af1fac9b8b95aada4b622230c2758ad8
SparXFusion <s2234nadar@gmail.com>
2019-11-09 16:01:30 +0530

    fix build

--- config/common.mk
+++ config/common.mk
@@ -2,7 +2,7 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/atom.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
-PRODUCT_BRAND ?= AtomOS
+PRODUCT_BRAND ?= Optimized LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
