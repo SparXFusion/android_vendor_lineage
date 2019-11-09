a690018efbe77a1e11f4db884de9f63248d7086f
SparXFusion <s2234nadar@gmail.com>
2019-11-09 15:57:37 +0530

    Initial Optlos vendor system

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,14 +1,9 @@
-# LineageOS System Version
+# Optimized LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
-
-# LineageOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
+    ro.optimized.version=$(LINEAGE_VERSION) \
+    ro.optimized.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.optimized.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(LINEAGE_VERSION) 
 
 # LineageOS Platform SDK Version
 ADDITIONAL_BUILD_PROPERTIES += \
